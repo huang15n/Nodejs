8e106074e1d04fba8a7f9634563b3879589dc836
Ed <huang15n@uwindsor.ca>
2024-11-17 22:23:57 -0500

    chapitre 3

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-const fs = require("fs");
+const { format } = require("date-fns"); // Importation de la fonction 'format' de 'date-fns'
+const { v4: uuidv4 } = require("uuid"); // Importation de la fonction 'v4' sous le nom 'uuidv4' depuis 'uuid'
+//// const { uuid } = require("uuid");
+// Affichage de la date actuelle dans le format spécifié
+console.log(format(new Date(), "yyyyMMdd\tHH:mm:ss"));
 
-if (!fs.existsSync("./nouvelle_doissier")) {
-  fs.mkdir("nouvelle_doissier", (err) => {
-    if (err) throw err;
-    console.log("Dossier 'nouvelle_doissier' créé");
-  });
-} else {
-  // Utilisation correcte de fs.rmdir avec un callback
-  fs.rmdir("./nouvelle_doissier", (err) => {
-    if (err) {
-      console.error("Erreur lors de la suppression du dossier :", err);
-    } else {
-      console.log("Dossier 'nouvelle_doissier' supprimé");
-    }
-  });
-}
+// Affichage d'un UUID généré
+console.log(uuidv4()); // Utilisation correcte de la fonction uuidv4
+
+console.log("Bonjour");
+
+// Pour démarrer le projet en mode développement, utilisez la commande suivante dans le terminal :
+console.log("npm run dev pour démarrer nodemon");
+
+// npm run dev to start nodenom
+
+// npm install date-fns
+
+console.log("c'est bon");
